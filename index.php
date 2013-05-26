a281e88317a51126ad2161e18e0a155030b875fd
prime9009 <90900909@mail.ru>
2013-05-26 20:48:56 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ header('Content-type: text/html; charset=utf-8');
 <title>Ноутбуки</title>
 </head>
 <body>
-<a href="note.php"> Заказать</a><br>
+<a href="note.php"> Перейти к выбору ноутбука и подсчета стоимости</a><br>
 <a href="list.php"> Просмотреть информацию о заказе </a>
 </body>
 </html>
