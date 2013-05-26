2f81be5ba43e57bfcc2d6b3eba9f7ae7c82d8f91
prime9009 <90900909@mail.ru>
2013-05-26 20:47:36 +0300

    Update notes.php

--- notes.php
+++ notes.php
@@ -3,17 +3,17 @@ $name=$_REQUEST['name'];
 echo "name=$name <br>"; //imya
 $tel=$_REQUEST['tel'];
 echo "tel=$tel <br>"; //telephone
-$adres=$_REQUEST['adres'];
-echo "adres=$adres <br>"; //telephone
-$Horoda=$_REQUEST['Horoda'];
-echo "Horoda=$Horoda <br>"; //misto
+$adress=$_REQUEST['adress'];
+echo "adress=$adress <br>"; //telephone
+$Goroda=$_REQUEST['Goroda'];
+echo "Goroda=$Goroda <br>"; //misto
 $kilkist=$_REQUEST['kilkist'];
 echo "kilkist=$kilkist <br>"; //kilkist
 $note=$_REQUEST['note'];
 echo "note=$note <br>"; //typ note
 $result=$_REQUEST['result'];
 echo "result=$result <br>"; // text rozrahunka
-$fp=fopen("zakaz.txt","a");
-fputs($fp,"$name\t$tel\t$adres\t$Horoda\t$kilkist\t$result\t");
+$fp=fopen("zam.txt","a");
+fputs($fp,"$name\t$tel\t$adress\t$Goroda\t$kilkist\t$result\t");
 fclose($fp);
 ?>
