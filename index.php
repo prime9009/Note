a779b89a0c07ee5d4dec8273aafe3d78d7d41407
prime9009 <90900909@mail.ru>
2013-05-26 18:02:22 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Ноутбуки</title>
+</head>
+<body>
+<a href="note.php"> Заказать</a><br>
+<a href="list.php"> Просмотреть информацию о заказе </a>
+</body>
+</html>
