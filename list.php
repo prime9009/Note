6defa34f75cd283f9f11c06b9f55e5312d305fee
prime9009 <90900909@mail.ru>
2013-05-27 16:52:14 +0400

    Update list.php

--- list.php
+++ list.php
@@ -1,8 +1,6 @@
 <?php
 header('Content-type: text/html; charset=utf-8');
 ?>
-
-
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -10,22 +8,21 @@ header('Content-type: text/html; charset=utf-8');
 </head>
 <body>
 <H1> Заказ ноутбуков </H1>
-<table border=2>
-<tr> <td>name</td><td>telephone</td><td>address</td> <td>Goroda</td> <td>kilkist</td>  </tr>
+<table border=10>
+<tr> <td>Имя</td><td>Телефон</td><td>Адресс</td> <td>Фирма</td> <td>Город</td> <td>Количество(штук)</td> <td>Цвет</td> <td>Размер дисплея</td> </tr>
 <?php
-$fp=fopen("zakaz.txt","r");
+$fp=fopen("zam.txt","r");
 if ($fp)
-
    {
        while(!feof($fp))
 
     {
        $stroka=fscanf($fp,"%s\t%s\t%s\t%s\t%d\t%s\n");
-       list($name,$tel,$adresss,$Goroda,$kilkist,$result)=$stroka;
+       list($name,$tel,$firma,$adresss,$Goroda,$kilkist,$Colour,$ScreenSize,$result)=$stroka;
         //$Goroda=fscanf($fp,"%s\t");
         //$kilkist=fscanf($fp,"%d\t");
         //$result=fscanf($fp,"%d\n");
-echo "<TR><td>$name</td> <td>$tel</td><td>$adresss</td><td> $Goroda</td> <td>$kilkist</td> </TR>";
+echo "<TR><td>$name</td> <td> $firma</td> <td>$tel</td><td>$adresss</td><td> $Goroda</td> <td>$kilkist</td> <td>$Colour</td> <td>$ScreenSize</td> </TR>";
 }
 fclose($fp);
 
