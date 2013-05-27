57596a55c490a1e9b950ec3859f980a8e226dc5e
prime9009 <90900909@mail.ru>
2013-05-27 16:52:32 +0400

    Update note.php

--- note.php
+++ note.php
@@ -5,26 +5,18 @@ header('Content-type: text/html; charset=utf-8');
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-
-
-
 <H1>
 <center>
 Добро пожаловать на страничку заказа интернет магазина "TestRozetka.ck.ua"
 </center>
 </H1>
-
 <form name=myform action="notes.php">
-
 </head>
 <body>
-<?php
-<body background='seryy-fon.jpg'>
-
+<p>
 Введите ФИО:
 <input type="text" name = "name" size="50" /><br /><br />
-
-
+</p>
 <p>
 <center><b>Выберите фирму желаемого ноутбука:</b> </center>
 </p>
@@ -33,7 +25,8 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name="Firma" value="1" checked><b>Asus <U>Цены: <br> от 17364 до 21435</U></b>
+
+	<input  type="radio" name="Firma" value="Asus" check><b>Asus <U>Цены: <br> от 17364 до 21435</U></b>
 	</td>
     <td>
 	<img src="asus.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="ASUS Corporation">
@@ -48,7 +41,7 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>Acer <U>Цены: <br> от 15678 до 19315</U></b>
+	<input  type="radio" name=" Firma" value="Acer" checked><b>Acer <U>Цены: <br> от 15678 до 19315</U></b>
 	</td>
     <td>
 	<img src="acer.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="ACER Corporation">
@@ -63,10 +56,10 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>Samsung <U>Цены: <br> от 22312 до 29999</U></b>
+	<input  type="radio" name=" Firma" value="Samsung" checked><b>Samsung <U>Цены: <br> от 22312 до 29999</U></b>
 	</td>
     <td>
-	<img src="samsung.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="SAMSUNG Corporation">
+	<img src="samsung.jpg" align="left" width="220" height="150" hspace="28" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="SAMSUNG Corporation">
 	<img src="samsung/s1.jpg" align="left" width="220" height="150">
 	<img src="samsung/s2.jpg" align="left" width="220" height="150">
 	<img src="samsung/s3.jpg" align="left" width="220" height="150">
@@ -78,7 +71,7 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>HP <U>Цены: <br> от 18411 до 21312</U></b>
+	<input  type="radio" name=" Firma" value="HP" checked><b>HP <U>Цены: <br> от 18411 до 21312</U></b>
 	</td>
     <td>
 	<img src="hp.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="HP Corporation">
@@ -93,7 +86,7 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>Sony <U>Цены: <br> от 17482 до 23879</U></b>
+	<input  type="radio" name=" Firma" value="Sony" checked><b>Sony <U>Цены: <br> от 17482 до 23879</U></b>
 	</td>
     <td>
 	<img src="sony.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="Sony Corporation">
@@ -108,7 +101,7 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>Apple <U>Цены: <br> от 30212 до 39218</U></b>
+	<input  type="radio" name=" Firma" value="Apple" checked><b>Apple <U>Цены: <br> от 30212 до 39218</U></b>
 	</td>
     <td>
 	<img src="apple.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="Apple Corporation">
@@ -123,7 +116,7 @@ header('Content-type: text/html; charset=utf-8');
 <table>
 <tr>
     <td>
-	<input  type="radio" name=" Firma" value="1" checked><b>DELL <U>Цены:<br> от 14337 до 16257</U></b>
+	<input  type="radio" name=" Firma" value="DELL" check><b>DELL <U>Цены:<br> от 14337 до 16257</U></b>
 	</td>
     <td>
 	<img src="dell.jpg" align="left" width="220" height="150" hspace="45" style="float: left; margin-right: 5px; margin-bottom: 0px;" alt="DELL Corporation">
@@ -140,27 +133,27 @@ header('Content-type: text/html; charset=utf-8');
 <U><b>Диагональ дисплея:</b> </U>
  </p>
 <p>
-<input  type="radio" name="ScreenSize" value="1" checked><b>15 Asus <U>(17364)</U>; Acer <U>(15678)</U>; Samsung <U>(22312)</U>; HP <U>(18411)</U>; Sony<U>(17482)</U>; Apple <U>(30212)</U>; DELL <U>(14337)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>16 Asus <U>(17671)</U>; Acer <U>(16745)</U>; Samsung <U>(23674)</U>; HP <U>(18719)</U>; Sony<U>(18620)</U>; Apple <U>(30874)</U>; DELL <U>(14687)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>17 Asus <U>(18235)</U>; Acer <U>(17104)</U>; Samsung <U>(24536)</U>; HP <U>(19543)</U>; Sony<U>(19540)</U>; Apple <U>(32015)</U>; DELL <U>(15006)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>18 Asus <U>(18843)</U>; Acer <U>(17721)</U>; Samsung <U>(25123)</U>; HP <U>(19984)</U>; Sony<U>(20530)</U>; Apple <U>(33817)</U>; DELL <U>(15218)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>19 Asus <U>(19279)</U>; Acer <U>(18236)</U>; Samsung <U>(26555)</U>; HP <U>(20090)</U>; Sony<U>(21463)</U>; Apple <U>(35515)</U>; DELL <U>(15612)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>20 Asus <U>(20023)</U>; Acer <U>(18987)</U>; Samsung <U>(28100)</U>; HP <U>(20672)</U>; Sony<U>(22365)</U>; Apple <U>(37756)</U>; DELL <U>(16019)</U>;</b><br>
-<input  type="radio" name="ScreenSize" value="1" checked><b>21 Asus <U>(21435)</U>; Acer <U>(19315)</U>; Samsung <U>(29999)</U>; HP <U>(21312)</U>; Sony<U>(23879)</U>; Apple <U>(39218)</U>; DELL <U>(16257)</U>.</b><br>
+<input  type="radio" name="ScreenSize" value="15" checked><b>15 Asus <U>(17364)</U>; Acer <U>(15678)</U>; Samsung <U>(22312)</U>; HP <U>(18411)</U>; Sony<U>(17482)</U>; Apple <U>(30212)</U>; DELL <U>(14337)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="16" checked><b>16 Asus <U>(17671)</U>; Acer <U>(16745)</U>; Samsung <U>(23674)</U>; HP <U>(18719)</U>; Sony<U>(18620)</U>; Apple <U>(30874)</U>; DELL <U>(14687)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="17" checked><b>17 Asus <U>(18235)</U>; Acer <U>(17104)</U>; Samsung <U>(24536)</U>; HP <U>(19543)</U>; Sony<U>(19540)</U>; Apple <U>(32015)</U>; DELL <U>(15006)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="18" checked><b>18 Asus <U>(18843)</U>; Acer <U>(17721)</U>; Samsung <U>(25123)</U>; HP <U>(19984)</U>; Sony<U>(20530)</U>; Apple <U>(33817)</U>; DELL <U>(15218)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="19" checked><b>19 Asus <U>(19279)</U>; Acer <U>(18236)</U>; Samsung <U>(26555)</U>; HP <U>(20090)</U>; Sony<U>(21463)</U>; Apple <U>(35515)</U>; DELL <U>(15612)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="20" checked><b>20 Asus <U>(20023)</U>; Acer <U>(18987)</U>; Samsung <U>(28100)</U>; HP <U>(20672)</U>; Sony<U>(22365)</U>; Apple <U>(37756)</U>; DELL <U>(16019)</U>;</b><br>
+<input  type="radio" name="ScreenSize" value="21" checked><b>21 Asus <U>(21435)</U>; Acer <U>(19315)</U>; Samsung <U>(29999)</U>; HP <U>(21312)</U>; Sony<U>(23879)</U>; Apple <U>(39218)</U>; DELL <U>(16257)</U>.</b><br>
 </p>
 
 <p>
 <U><b>Выберите желаемый цвет ноутбука:</b></U>
 </p>
 
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Черный<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Белый<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Металик<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Красный<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Желтый<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Фиолетовый<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Зеленый<br>
-<input type="radio" value="1" id="chek1" onclick="checkCheck(this)" name="colour" />Синий<br>
+<input type="radio" value="Чёрный" id="chek1" onclick="checkCheck(this)" name="colour" />Черный<br>
+<input type="radio" value="Белый" id="chek1" onclick="checkCheck(this)" name="colour" />Белый<br>
+<input type="radio" value="Металик" id="chek1" onclick="checkCheck(this)" name="colour" />Металик<br>
+<input type="radio" value="Красный" id="chek1" onclick="checkCheck(this)" name="colour" />Красный<br>
+<input type="radio" value="Желтый" id="chek1" onclick="checkCheck(this)" name="colour" />Желтый<br>
+<input type="radio" value="Фиолетовый" id="chek1" onclick="checkCheck(this)" name="colour" />Фиолетовый<br>
+<input type="radio" value="Зелёный" id="chek1" onclick="checkCheck(this)" name="colour" />Зеленый<br>
+<input type="radio" value="Синий" id="chek1" onclick="checkCheck(this)" name="colour" />Синий<br>
 
 
 
@@ -171,19 +164,19 @@ header('Content-type: text/html; charset=utf-8');
 
 Выберите город:
 <select name="Goroda">
-<option selected value="Kyiv">Киев</option>
-<option value="Lvov">Львов</option>
-<option value="Harkov">Харьков</option>
-<option value="Shutomur">Житомир</option>
-<option value="Sumy">Сумы</option>
-<option value="Cherkassy">Черкассы</option>
-<option value="Odessa">Одесса</option>
-<option value="Chernigov">Чернигов</option>
-<option value="Poltava">Полтава</option>
-<option value="Kirovograd">Кировоград</option>
-<option value="Krivoi Rog">Кривой Рог</option>
-<option value="Yalta">Ялта</option>
-<option value="Kherson">Херсон</option>
+<option selected value="Киев">Киев</option>
+<option value="Львов">Львов</option>
+<option value="Харьков">Харьков</option>
+<option value="Житомир">Житомир</option>
+<option value="Сумы">Сумы</option>
+<option value="Черкассы">Черкассы</option>
+<option value="Одесса">Одесса</option>
+<option value="Чернигов">Чернигов</option>
+<option value="Полтава">Полтава</option>
+<option value="Кировоград">Кировоград</option>
+<option value="Кривой Рог">Кривой Рог</option>
+<option value="Ялта">Ялта</option>
+<option value="Херсон">Херсон</option>
 <select>
 <br><br />
 
@@ -315,6 +308,6 @@ alert();
 <input type="reset" value="Отмена ">
 </p>
 </form>
-?>
+
 </body>
 </html>
